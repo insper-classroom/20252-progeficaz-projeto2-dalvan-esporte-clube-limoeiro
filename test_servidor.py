be05c4b438bc2f929864cd3a7351a83886dc107d
Rafaelvlf <rafaelvlf1@al.insper.edu.br>
2025-09-12 17:15:19 -0300

    feat: teste pega imoveis por tipo

--- test_servidor.py
+++ test_servidor.py
@@ -154,3 +154,45 @@ def test_cria_imovel_db(mock_connect_db):
     )
     mock_conn.commit.assert_called_once()
     mock_conn.close.assert_called_once()
+
+@patch("utils.connect_db")
+def test_lista_imovel_por_tipo(mock_connect_db, client):
+    """Testa a rota GET /imoveis/tipo/<tipo> sem acessar o banco de dados real"""
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+    mock_connect_db.return_value = mock_conn
+    mock_cursor.fetchall.return_value = [
+        (1, 'Nicole Common', 'Travessa', 'Lake Danielle', 'Judymouth', '85184', 'casa em condominio', 488423.52, '2017-07-29'),
+        (2, 'Price Prairie', 'Travessa', 'Colonton', 'North Garyville', '93354', 'casa em condominio', 260069.89, '2021-11-30'),
+    ]
+    response = client.get("/imoveis/tipo/casa em condominio")
+    assert response.status_code == 200
+    expected_response = {
+        "imoveis": [
+            {
+                "id": 1,
+                "logradouro": "Nicole Common",
+                "tipo_logradouro": "Travessa",
+                "bairro": "Lake Danielle",
+                "cidade": "Judymouth",
+                "cep": "85184",
+                "tipo": "casa em condominio",
+                "valor": 488423.52,
+                "data_aquisicao": "2017-07-29"
+            },
+            {
+                "id": 2,
+                "logradouro": "Price Prairie",
+                "tipo_logradouro": "Travessa",
+                "bairro": "Colonton",
+                "cidade": "North Garyville",
+                "cep": "93354",
+                "tipo": "casa em condominio",
+                "valor": 260069.89,
+                "data_aquisicao": "2021-11-30"
+            }
+        ]
+    }
+    assert response.get_json() == expected_response
+    
