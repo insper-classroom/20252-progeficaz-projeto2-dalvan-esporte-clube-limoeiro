294a4e41ff3adfc7d157a62711f0803230059d4a
Rafaelvlf <rafaelvlf1@al.insper.edu.br>
2025-09-12 14:52:14 -0300

    feat: rota de imóvel por id

--- servidor.py
+++ servidor.py
@@ -27,3 +27,25 @@ def listar_imoveis():
         })
 
     return jsonify({"imoveis": imoveis})
+
+@app.route("/imoveis/<id>", methods=["GET"])
+def get_imovel_por_id(id):
+    conn = utils.connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM imoveis WHERE id =?",(id,))
+    rows = cursor.fetchall()
+    if not rows:
+        return jsonify({"imovel": None}), 404
+    row = rows[0]
+    imovel = {
+        "id": row[0],
+        "logradouro": row[1],
+        "tipo_logradouro": row[2],
+        "bairro": row[3],
+        "cidade": row[4],
+        "cep": row[5],
+        "tipo": row[6],
+        "valor": row[7],
+        "data_aquisicao": row[8]
+    }
+    return jsonify({"imovel": imovel})
